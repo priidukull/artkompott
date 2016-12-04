24f54a8ddf0bcf6fd5709af233bc99dc4720536f
Priidu Kull <priidu.kull@gmail.com>
2016-12-04 12:28:12 +0200

    Routing to make the artist pages work

--- kompott/urls.py
+++ kompott/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^category/([a-zA-Z]+)/$', views.category, name='index'),
+    url(r'^artist/([a-zA-Z]+)/$', views.artist, name='index'),
 ]
